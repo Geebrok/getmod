2938cfa130b24b5d96e465f06cf84d304adc95b2
Grant Brooks <grantbrooks75@gmail.com>
2014-12-13 14:54:18 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Getmod::Application.config.secret_key_base = '823d388652dd5db727887614321e0b43990e16e34a908e9377be5ea18a7e10004e877f893bb69bba22378712c8fe2041dd327e406ad93a7a463877586053b674'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Getmod::Application.config.session_store :cookie_store, key: '_getmod_session'
